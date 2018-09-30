cde053842973d310fd221bd988334fc0a65aea19
Pratik Sah <shivam3448@gmail.com>
2018-10-01 02:35:33 +0530

    Add files via upload

---
+++ My WorkSpace/public/gatepass/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+$(document).ready(function () {
+    $('.slider').slider();
+});
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
+$(document).ready(function () {
+    $('.carousel').carousel();
+});
+$(document).ready(function(){
+    $('.modal').modal();
+});
